c18a4f311adc45e7e4d32dfcbd4b777ede36466e
jessiechn <183398897@qq.com>
2018-10-08 18:48:28 +0800

    third time pull

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+//import HelloWorld from '@/components/HelloWorld'
+import ShoppMail from '@/components/pages/ShoppingMail'
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name:'ShoppingMail',
+      component:ShoppMail
     }
   ]
-})
+}) 
